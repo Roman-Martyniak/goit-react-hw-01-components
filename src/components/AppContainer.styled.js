348b2cca7aa9c00a2319e541a655d4c569118ccc
Roman Martyniak <110495171+Roman-Martyniak@users.noreply.github.com>
2023-03-05 20:14:49 +0200

    #

---
+++ src/components/AppContainer.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  background-color: #034677;
+  padding: 20px;
+`;
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+import { ListOfFriends, Item } from './FriendList.styled';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ListOfFriends>
+      {friends.map(friend => (
+        <Item key={friend.id}>
+          <FriendListItem friend={friend} />
+        </Item>
+      ))}
+    </ListOfFriends>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
+export default FriendList;
